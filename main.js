b2ca1bc4d31f3425418a2a8b14d710e88da6cd04
Allan Fernandes <Fernzallan14@gmail.com>
2023-06-10 13:43:29 +0100

    strip-protocol

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-console.log('hello world');
+function normalizeURL(URLstring) {
+    
+    const { host, pathname } = new URL(URLstring);
+    const stripProtocal = `${host}${pathname}`
+    
+    if(stripProtocal.length > 0 && stripProtocal.slice(-1) === '/') {
+        return stripProtocal.slice(0,-1);
+    }
+    return stripProtocal;
+}
+
+normalizeURL('https://github.com/coreybutler/nvm-windows/releases/')
